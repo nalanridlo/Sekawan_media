3ff9b39d68cf505957c170b75c964a1287e50f97
nalanridlo <nalanridlof@student.ub.ac.id>
2023-11-23 14:04:57 +0700

    react-js

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,36 +1,51 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../assets/css/card.css";
-import img from "../assets/img/dc.png";
-import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
-function Card() {
-  const navigation = useNavigate();
+
+function Card({ item, navigation }) {
+  const ratingValue = item.rating;
+
   return (
-    <div className="card mb-3" style={{ width: "18rem" }}>
-      <img src={img} className="card-img-top" alt="..." />
-      <div className="card-body">
-        <div className="card-tittle">
-          <span className="">asd</span>
-        </div>
-        <div className="card-rating">
-          <span>2</span>
-        </div>
-        <div className="card-information">
-          <div>
-
-          <span className="">1</span>
-          <span className="">2</span>
+    <div className="card mb-5" style={{ width: "18rem" }}>
+        <img src={item.image} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <div className="card-tittle">
+            <h5 className="">{item.name}</h5>
           </div>
-          <span>3</span>
+          <div className="card-rating">
+            <div className="rating">
+              {[...Array(5)].map((_, index) => {
+                return (
+                  <FontAwesomeIcon
+                    key={index}
+                    icon={index < ratingValue ? faStar : ["far", "star"]}
+                    style={{
+                      color: index < ratingValue ? "#ffc107" : "#ccc",
+                    }}
+                  />
+                );
+              })}
+            </div>
+          </div>
+          <div className="card-information">
+            <div>
+              <span className="">{item.category} - </span>
+              <span className="">${item.price}</span>
+            </div>
+            <span>{item.status}</span>
+          </div>
         </div>
+        <a
+          className="btn btn-outline-primary pb-2"
+          onClick={() => {
+            navigation(`/detail/${item.id}`);
+          }}
+        >
+          Baca lebih lanjut
+        </a>
       </div>
-      <a className="btn btn-outline-primary pb-2"
-      onClick={() => {
-        navigation(`/detail/${item.id}`);
-      }
-      }
-      >Baca lebih lanjut</a>
-    </div>
   );
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -136,49 +136,9 @@ function Home() {
         <h1>All Restaurants</h1>
         <div className="row">
           {filteredRestaurant.map((item) => {
-            const ratingValue = item.rating;
             return (
               <div className="col-md-3" key={item.id}>
-                <div className="card mb-5" style={{ width: "18rem" }}>
-                  <img src={item.image} className="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <div className="card-tittle">
-                      <h5 className="">{item.name}</h5>
-                    </div>
-                    <div className="card-rating">
-                      <div className="rating">
-                        {[...Array(5)].map((_, index) => {
-                          return (
-                            <FontAwesomeIcon
-                              key={index}
-                              icon={
-                                index < ratingValue ? faStar : ["far", "star"]
-                              }
-                              style={{
-                                color: index < ratingValue ? "#ffc107" : "#ccc",
-                              }}
-                            />
-                          );
-                        })}
-                      </div>
-                    </div>
-                    <div className="card-information">
-                      <div>
-                        <span className="">{item.category} - </span>
-                        <span className="">${item.price}</span>
-                      </div>
-                      <span>{item.status}</span>
-                    </div>
-                  </div>
-                  <a
-                    className="btn btn-outline-primary pb-2"
-                    onClick={() => {
-                      navigation(`/detail/${item.id}`);
-                    }}
-                  >
-                    Baca lebih lanjut
-                  </a>
-                </div>
+                <Card item={item} navigation={navigation} />
               </div>
             );
           })}
